b4017d648430827bc351b424b646f7d11ea51231
Jon Peirce <jon.peirce@gmail.com>
2023-02-03 16:02:38 +0000

    version bump 0.0.2

--- psychopy_cedrus/__init__.py
+++ psychopy_cedrus/__init__.py
@@ -5,7 +5,7 @@
 # Copyright (C) 2002-2018 Jonathan Peirce (C) 2019-2022 Open Science Tools Ltd.
 # Distributed under the terms of the GNU General Public License (GPL).
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 from .cedrus import RB730
 from .cedrusBox import cedrusButtonBoxComponent  # plugin component for Builder
