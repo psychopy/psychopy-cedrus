8d8b3c949d38d6c50f81fcbf1e04de0952e9c547
TEParsons <todd.e.parsons@gmail.com>
2024-05-01 11:20:36 +0100

    SYS: Version bump 0.0.4

--- psychopy_cedrus/__init__.py
+++ psychopy_cedrus/__init__.py
@@ -5,6 +5,11 @@
 # Copyright (C) 2002-2018 Jonathan Peirce (C) 2019-2022 Open Science Tools Ltd.
 # Distributed under the terms of the GNU General Public License (GPL).
 
-__version__ = '0.0.2'
+import importlib.metadata
+
+try:
+    __version__ = importlib.metadata.version("psychopy-cedrus")
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "0.dev"
 
 from .cedrus import RB730
