4e659eb51a4073fbd82dca4bceab42cd52543cf3
Todd Parsons <todd.e.parsons@googlemail.com>
2024-05-01 17:50:56 +0100

    FF: Use correct name in CedrusButtonBoxComponent component

--- psychopy_cedrus/components/cedrusBox/__init__.py
+++ psychopy_cedrus/components/cedrusBox/__init__.py
@@ -50,7 +50,7 @@ class CedrusButtonBoxComponent(KeyboardComponent):
                  startType='time (s)', startVal=0.0,
                  stopType='duration (s)', stopVal=1.0,
                  startEstim='', durationEstim='',):
-        super(cedrusButtonBoxComponent, self).__init__(
+        super(CedrusButtonBoxComponent, self).__init__(
             exp, parentName, name=name,
             allowedKeys=allowedKeys, store=store, discardPrev=discardPrev,
             forceEndRoutine=forceEndRoutine, storeCorrect=storeCorrect,
