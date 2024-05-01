6a818219af7813dd75cdc4fedbd675d1ad00feaf
Todd Parsons <todd.e.parsons@googlemail.com>
2024-05-01 16:49:03 +0100

    FF: Capitalise C in test suite import

--- tests/test_builder/test_ExampleComponent.py
+++ tests/test_builder/test_ExampleComponent.py
@@ -1,4 +1,4 @@
-from psychopy_cedrus.components.cedrusBox import cedrusButtonBoxComponent
+from psychopy_cedrus.components.cedrusBox import CedrusButtonBoxComponent
 from psychopy.tests.test_experiment.test_components.test_base_components import BaseComponentTests
 
 class TestCedrusButtonBoxComponent(BaseComponentTests):
