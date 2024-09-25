36f5744b1e58f415b721120c01e5c8b4ca2cb7c0
TEParsons <todd.e.parsons@gmail.com>
2024-09-25 16:10:40 +0100

    NF: Add Riponda photodiode backend

--- psychopy_cedrus/components/riponda.py
+++ psychopy_cedrus/components/riponda.py
@@ -1,7 +1,79 @@
 from psychopy.experiment.components.buttonBox import ButtonBoxComponent
+from psychopy.experiment.routines.photodiodeValidator import PhotodiodeValidatorRoutine
 from psychopy.experiment import getInitVals, Param
 from psychopy.experiment.plugins import DeviceBackend
 from psychopy.localization import _translate
+
+
+class RipondaPhotodiodeValidatorBackend(DeviceBackend):
+    # which component is this backend for?
+    component = PhotodiodeValidatorRoutine
+    # what value should Builder use for this backend?
+    key = "riponda"
+    # what label should be displayed by Builder for this backend?
+    label = _translate("Cedrus Riponda")
+    # what hardware classes are relevant to this backend?
+    deviceClasses = ["psychopy_cedrus.riponda.RipondaPhotodiodeGroup"]
+
+    def getParams(self):
+        """
+        Get parameters from this backend to add to each new instance of ButtonBoxComponent
+
+        Returns
+        -------
+        dict[str:Param]
+            Dict of Param objects, which will be added to any Button Box Component's params, along with a dependency
+            to only show them when this backend is selected
+        list[str]
+            List of param names, defining the order in which params should appear
+        """
+        # define order
+        order = [
+            "ripondaDeviceNo",
+            "ripondaNChannels"
+        ]
+        # define params
+        params = {}
+
+        params['ripondaDeviceNo'] = Param(
+            "0", valType="code", inputType="single", categ="Device",
+            label=_translate("Device number"),
+            hint=_translate(
+                "Number of the device to connect to (starts at 0)"
+            )
+        )
+        params['ripondaNChannels'] = Param(
+            2, valType="code", inputType="single", categ="Device",
+            label=_translate("Num. diodes"),
+            hint=_translate(
+                "How many diodes this device has."
+            )
+        )
+
+        return params, order
+
+    def addRequirements(self):
+        """
+        Add any required module/package imports for this backend
+        """
+        self.exp.requireImport(
+            importName="riponda",
+            importFrom="psychopy_cedrus"
+        )
+
+    def writeDeviceCode(self, buff):
+        # get inits
+        inits = getInitVals(self.params)
+        # make ButtonGroup object
+        code = (
+            "deviceManager.addDevice(\n"
+            "    deviceClass='psychopy_cedrus.riponda.RipondaPhotodiodeGroup',\n"
+            "    deviceName=%(deviceLabel)s,\n"
+            "    pad=%(ripondaDeviceNo)s,\n"
+            "    channels=%(ripondaNChannels)s,\n"
+            ")\n"
+        )
+        buff.writeOnceIndentedLines(code % inits)
 
 
 class RipondaButtonBoxBackend(DeviceBackend):
