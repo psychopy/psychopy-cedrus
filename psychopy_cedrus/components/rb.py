324179c3ff57c0ad5de84c67991ec89e5f4bb981
TEParsons <todd.e.parsons@gmail.com>
2024-10-02 16:49:05 +0100

    RF: Use a base backend code for all Xid devices in Builder

--- psychopy_cedrus/components/rb.py
+++ psychopy_cedrus/components/rb.py
@@ -1,8 +1,8 @@
 from psychopy.experiment.components.buttonBox import ButtonBoxComponent
 from psychopy.experiment.routines.photodiodeValidator import PhotodiodeValidatorRoutine
-from psychopy.experiment import getInitVals, Param
 from psychopy.experiment.plugins import DeviceBackend
 from psychopy.localization import _translate
+from . import util
 
 
 class RBPhotodiodeValidatorBackend(DeviceBackend):
@@ -14,43 +14,9 @@ class RBPhotodiodeValidatorBackend(DeviceBackend):
     label = _translate("Cedrus RB Series")
     # what hardware classes are relevant to this backend?
     deviceClasses = ["psychopy_cedrus.rb.RBPhotodiodeGroup"]
-    
+
     def getParams(self):
-        """
-        Get parameters from this backend to add to each new instance of ButtonBoxComponent
-
-        Returns
-        -------
-        dict[str:Param]
-            Dict of Param objects, which will be added to any Button Box Component's params, along with a dependency
-            to only show them when this backend is selected
-        list[str]
-            List of param names, defining the order in which params should appear
-        """
-        # define order
-        order = [
-            "rbDeviceNo",
-            "rbNChannels"
-        ]
-        # define params
-        params = {}
-
-        params['rbDeviceNo'] = Param(
-            "", valType="str", inputType="single", categ="Device",
-            label=_translate("Device number"),
-            hint=_translate(
-                "Number of the device to connect to (starts at 0)"
-            )
-        )
-        params['rbNChannels'] = Param(
-            2, valType="code", inputType="single", categ="Device",
-            label=_translate("Num. diodes"),
-            hint=_translate(
-                "How many diodes this device has."
-            )
-        )
-
-        return params, order
+        return util.getXidPhotodiodeParams(key="rb")
 
     def addRequirements(self):
         """
@@ -60,20 +26,14 @@ class RBPhotodiodeValidatorBackend(DeviceBackend):
             importName="rb",
             importFrom="psychopy_cedrus"
         )
-
+    
     def writeDeviceCode(self, buff):
-        # get inits
-        inits = getInitVals(self.params)
-        # make ButtonGroup object
-        code = (
-            "deviceManager.addDevice(\n"
-            "    deviceClass='psychopy_cedrus.rb.RBPhotodiodeGroup',\n"
-            "    deviceName=%(deviceLabel)s,\n"
-            "    pad=%(rbDeviceNo)s,\n"
-            "    channels=%(rbNChannels)s,\n"
-            ")\n"
+        return util.writeXidPhotodiodeCode(
+            self,
+            buff,
+            cls="psychopy_cedrus.rb.RBPhotodiodeGroup",
+            key="rb"
         )
-        buff.writeOnceIndentedLines(code % inits)
 
 
 class RBButtonBoxBackend(DeviceBackend):
@@ -82,50 +42,23 @@ class RBButtonBoxBackend(DeviceBackend):
     other ButtonBoxBackends.
     """
     key = "rb"
-    label = "Cedrus RB Series"
+    label = _translate("Cedrus RB Series")
     component = ButtonBoxComponent
     deviceClasses = ["psychopy_cedrus.rb.RBButtonGroup"]
 
-    def getParams(self: ButtonBoxComponent):
-        # define order
-        order = [
-            "rbIndex",
-            "rbNButtons"
-        ]
-        # define params
-        params = {}
-        params['rbIndex'] = Param(
-            0, valType='int', inputType="single", categ='Device',
-            label=_translate("Device number"),
-            hint=_translate(
-                "Device number, if you have multiple devices which one do you want (0, 1, 2...)"
-            )
-        )
-        params['rbNButtons'] = Param(
-            7, valType="code", inputType="single", categ="Device",
-            label=_translate("Num. buttons"),
-            hint=_translate(
-                "How many buttons this button box has."
-            )
-        )
-
-        return params, order
+    def getParams(self):
+        return util.getXidButtonBoxParams(key="rb")
 
     def addRequirements(self: ButtonBoxComponent):
         self.exp.requireImport(
-            importName="rb", importFrom="psychopy_cedrus"
+            importName="rb", 
+            importFrom="psychopy_cedrus"
         )
-
+    
     def writeDeviceCode(self, buff):
-        # get inits
-        inits = getInitVals(self.params)
-        # make ButtonGroup object
-        code = (
-            "deviceManager.addDevice(\n"
-            "    deviceClass='psychopy_cedrus.rb.RBButtonGroup',\n"
-            "    deviceName=%(deviceLabel)s,\n"
-            "    pad=%(rbIndex)s,\n"
-            "    channels=%(rbNButtons)s\n"
-            ")\n"
+        return util.writeXidButtonBoxCode(
+            self,
+            buff,
+            cls="psychopy_cedrus.rb.RBButtonGroup",
+            key="rb"
         )
-        buff.writeOnceIndentedLines(code % inits)
