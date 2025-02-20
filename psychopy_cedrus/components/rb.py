66cf1e98bfbfcc61b073d7316c944c1aa8d5c748
todd <todd.e.parsons@gmail.com>
2025-02-20 16:09:39 +0000

    FF: Add audio validator backend for RB series

--- psychopy_cedrus/components/rb.py
+++ psychopy_cedrus/components/rb.py
@@ -1,8 +1,26 @@
 from psychopy.experiment.components.buttonBox import ButtonBoxComponent
 from psychopy.experiment.routines.visualValidator import VisualValidatorRoutine
-from psychopy.experiment.plugins import DeviceBackend
+from psychopy.experiment.plugins import DeviceBackend, PluginDevicesMixin
 from psychopy.localization import _translate
 from . import util
+
+# import Component/Routine classes in a version-safe way
+try:
+    from psychopy.experiment.components.buttonBox import ButtonBoxComponent
+except ImportError:
+    ButtonBoxComponent = PluginDevicesMixin
+try:
+    from psychopy.experiment.routines.visualValidator import VisualValidatorRoutine
+except ImportError:
+    VisualValidatorRoutine = PluginDevicesMixin
+try:
+    from psychopy.experiment.routines.audioValidator import AudioValidatorRoutine
+except ImportError:
+    AudioValidatorRoutine = PluginDevicesMixin
+try:
+    from psychopy.experiment.components.soundsensor import SoundSensorComponent
+except ImportError:
+    SoundSensorComponent = PluginDevicesMixin
 
 
 class RBVisualValidatorBackend(DeviceBackend):
@@ -33,6 +51,34 @@ class RBVisualValidatorBackend(DeviceBackend):
         )
 
 
+class RBAudioValidatorBackend(DeviceBackend):
+    # which component is this backend for?
+    component = AudioValidatorRoutine
+    # what value should Builder use for this backend?
+    key = "rb"
+    # what label should be displayed by Builder for this backend?
+    label = _translate("Cedrus RB Series")
+    # what hardware classes are relevant to this backend?
+    deviceClasses = ["psychopy_cedrus.rb.RBSoundSensorGroup"]
+
+    def getParams(self):
+        return util.getXidSoundSensorParams(key="rb")
+
+    def addRequirements(self):
+        """
+        Add any required module/package imports for this backend
+        """
+        return
+    
+    def writeDeviceCode(self, buff):
+        return util.writeXidSoundSensorCode(
+            self, 
+            buff, 
+            cls="psychopy_cedrus.rb.RBSoundSensorGroup",
+            key="rb"
+        )
+
+
 class RBButtonBoxBackend(DeviceBackend):
     """
     Adds a basic serial connection backend for ButtonBoxComponent, as well as acting as an example for implementing
@@ -46,7 +92,7 @@ class RBButtonBoxBackend(DeviceBackend):
     def getParams(self):
         return util.getXidButtonBoxParams(key="rb")
 
-    def addRequirements(self: ButtonBoxComponent):
+    def addRequirements(self):
         self.exp.requireImport(
             importName="rb", 
             importFrom="psychopy_cedrus"
@@ -57,5 +103,28 @@ class RBButtonBoxBackend(DeviceBackend):
             self,
             buff,
             cls="psychopy_cedrus.rb.RBButtonGroup",
+            key="rb"
+        )
+
+
+class RipondaSoundSensorBackend(DeviceBackend):
+    key = "rb"
+    label = _translate("Cedrus RB Series")
+    component = SoundSensorComponent
+    deviceClasses = ["psychopy_cedrus.rb.RBSoundSensorGroup"]
+
+    def getParams(self):
+        return util.getXidSoundSensorParams("rb")
+
+    def addRequirements(self):
+        self.exp.requireImport(
+            importName="rb", importFrom="psychopy_cedrus"
+        )
+
+    def writeDeviceCode(self, buff):
+        return util.writeXidLightSensorCode(
+            self,
+            buff,
+            cls="psychopy_cedrus.rb.RBLightSensorGroup",
             key="rb"
         )
